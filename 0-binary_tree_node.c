01b3e13ce1f0c1cfaa508c8af7370177be7f1190
Frankblation <Frankblation@gmail.com>
2023-08-17 17:15:14 -0700

    1st commit new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+    if (result != NULL)
+    return (NULL);
+    {
+        result->left = NULL;
+        result->right = NULL;
+        result->n = value;
+        result->parent = parent;
+    }
+    return (result);
+}
