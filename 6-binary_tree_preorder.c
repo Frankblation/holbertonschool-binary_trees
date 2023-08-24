1a2b3f28b980029237a11c83c7895edb964860fd
Frankblation <Frankblation@gmail.com>
2023-08-23 21:16:08 -0700

    work

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder- go through a binary tree using pre-order traversal
+ *@tree: binary_tree_t
+ *@func: int
+ *
+ * Return: 0
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
